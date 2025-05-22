db174b4d57e88695054815de5dbf601ed5431be4
raphaeldemilfhunter <165992591+raphaelhuerzele@users.noreply.github.com>
2025-05-22 11:25:21 +0200

    Manuell Patch 0016 - Berechne U & P aus I & R

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -16,13 +16,13 @@ export default function Formelrad() {
         const i = parseFloat(values.i);
         const r = parseFloat(values.r);
 
-        // Fall 1: P und I → U berechnen
+        // Fall 1: P und I → U
         if (!isNaN(p) && !isNaN(i)) {
             const u = p / i;
             setValues(values => ({ ...values, u: u.toFixed(2) }));
         }
 
-        // Fall 2: P und R → U und I berechnen
+        // Fall 2: P und R → U & I
         else if (!isNaN(p) && !isNaN(r)) {
             const u = Math.sqrt(p * r);
             const i = Math.sqrt(p / r);
@@ -30,6 +30,17 @@ export default function Formelrad() {
                 ...values,
                 u: u.toFixed(2),
                 i: i.toFixed(2)
+            }));
+        }
+
+        // Fall 3: I und R → U & P
+        else if (!isNaN(i) && !isNaN(r)) {
+            const u = i * r;
+            const p = i * i * r;
+            setValues(values => ({
+                ...values,
+                u: u.toFixed(2),
+                p: p.toFixed(2)
             }));
         }
     }
